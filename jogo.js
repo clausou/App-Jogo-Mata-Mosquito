6bd760e08f3370ad5fec296f249f9a5d47595ed7
clausou <clausou@hotmail.com>
2020-09-28 18:51:14 -0300

    função Game Over e Reiniciar

--- jogo.js
+++ jogo.js
@@ -21,8 +21,8 @@ function posicaoRandomica() {
 		document.getElementById('mosquito').remove()
 
 		if(vidas > 3) {
-			alert('interromper o jogo (game over)')
 
+			window.location.href = 'fim_de_jogo.html'
 		} else {
 			document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
 			vidas++

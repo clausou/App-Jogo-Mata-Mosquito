5e44d8aa5d70c0d866a6d17a2b471625452676ab
clausou <clausou@hotmail.com>
2020-09-28 16:48:48 -0300

    Inclusão da lógica de criação dos mosquisto de forma randômica

--- jogo.js
+++ jogo.js
@@ -1,12 +1,40 @@
 var altura = 0
 var largura = 0
 
+//DIMENSIONAMENTO DA TELA ONDE O APLICATIVO ESTÁ RODANDO
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
 	largura = window.innerWidth
+
 	console.log(altura, largura)
 }
 
 ajustaTamanhoPalcoJogo()
+
+
+//POSICIONAMENTO DO MOSQUISTO DE FORMA RANDÔMICA
+function posicaoRandomica() {
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+ 
+	console.log(posicaoX, posicaoY)
+
+	//CRIAR O ELEMENTO HTMAL DE FORMA PROGRAMADA
+	var mosquito = document.createElement('img')
+	mosquito.src = 'imagens/mosquito.png'
+	mosquito.className = 'mosquito1'
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+
+	document.body.appendChild(mosquito)
+}
+
+
+
+
 
 
